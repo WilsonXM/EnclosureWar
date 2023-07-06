84ebff936903b778af849bf9ee374b60616f0029
HPwilson <xuming_formal@foxmail.com>
2023-07-06 19:52:09 +0800

    create the Qt project structure

---
+++ build-EnclosureWar-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/CMakeFiles/EnclosureWar_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EnclosureWar_autogen"
+  "EnclosureWar_autogen/include/ui_mainwindow.h"
+  "EnclosureWar_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/EnclosureWar_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
