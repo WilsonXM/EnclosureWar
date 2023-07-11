86d50b406f9852da0f345cb7a522873d258a8c52
Zhang Shuqi <2193995698@qq.com>
2023-07-11 22:16:01 +0800

    first commit

---
+++ EnclosureWar_Final/Common/Color.h
@@ -0,0 +1,18 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+enum Color
+{
+    WHITE = 0,
+    RED,
+    BLUE,
+    GREEN,
+    PURPLE,
+    LIGHTRED,
+    LIGHTBLUE,
+    LIGHTGREEN,
+    LIGHTPURPLE,
+    Gray
+};
+
+#endif // COLOR_H
